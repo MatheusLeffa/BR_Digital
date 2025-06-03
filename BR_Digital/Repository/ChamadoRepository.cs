c436e11507622c84a915b262f8119cd70aad6b4b
Matt_Leffa <83082324+MatheusLeffa@users.noreply.github.com>
2025-06-03 01:56:14 -0300

    .

--- BR_Digital/Repository/ChamadoRepository.cs
+++ BR_Digital/Repository/ChamadoRepository.cs
@@ -25,17 +25,19 @@ namespace BR_Digital.Repository
         {
             var rand = new Random();
             var clientes = new[] { new Cliente("VIVO"), new Cliente("TIM"), new Cliente("CLARO"), new Cliente("OI") };
-            var tiposChamado = new[] { "Backbone-Tx", "Backbone-Rx" };
-            var statusChamado = new[] { "Critico", "Importante", "Normal", "Resolvido" };
+            var tiposChamado = new[] { "Backbone-Tx", "Backbone-Rx", "Noc-Tx" };
+            var statusChamado = new[] { "Critico", "Importante", "Normal", "Concluido" };
+            var trechosChamado = new[] { "Cachoeirinha / Gravataí", "Porto Alegre / Cachoeirinha", "Porto Alegre / Alvorada", "Porto Alegre / Canoas" };
 
             for (int i = 0; i < 100; i++)
             {
                 var cliente = clientes[rand.Next(0, clientes.Length)];
                 var tipoChamado = tiposChamado[rand.Next(0, tiposChamado.Length)];
                 var status = statusChamado[rand.Next(0, statusChamado.Length)];
+                var trechos = trechosChamado[rand.Next(0, trechosChamado.Length)];
                 var data = new DateTime(rand.Next(2020, 2026), rand.Next(1, 13), rand.Next(1, 29), rand.Next(0, 24), rand.Next(0, 60), rand.Next(0, 60), DateTimeKind.Utc);
 
-                var chamado = new Chamado(cliente, "este / aquele", tipoChamado, status, data);
+                var chamado = new Chamado(cliente, trechos, tipoChamado, status, data);
                 ListaChamados.Add(chamado);
             }
         }
